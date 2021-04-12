64bf13e3f83389e900b4bbed59b902ced0613c96
Vladimir <79061188133@ya.ru>
2021-04-12 20:29:31 +0300

    Подготовка ко второму заданию

---
+++ les02/var01.py
@@ -0,0 +1,14 @@
+import pprint
+import bs4 as bs
+import requests as rs
+
+
+class Get_HH():
+    def get_data(self):
+        pass
+
+    def processing(self):
+        pass
+
+    def save_data(self):
+        pass
