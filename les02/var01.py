9b0c6ca8f8fa9b3d48116f342a4cdf9f13ae689c
Vladimir <79061188133@ya.ru>
2021-04-13 22:19:24 +0300

    Хехе.ру закончен

--- les02/var01.py
+++ les02/var01.py
@@ -5,14 +5,20 @@ import pickle
 import json
 import time
 
-url_hh = 'https://hh.ru/search/vacancy?area=1&fromSearchLine=true&st=searchVacancy&text=Python&from=suggest_post'
+# url_search = 'https://hh.ru/search/vacancy?area=1&fromSearchLine=true&st=searchVacancy&text=Python&from=suggest_post'
+url_search = 'https://hh.ru/search/vacancy?clusters=true&area=1&ored_clusters=true&enable_snippets' \
+             '=true&salary=&st=searchVacancy&text=python'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
                          '(KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'}
 
 
-class GetHH():
-    def __init__(self):
+class GetHH:
+    def __init__(self, name):
+        self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                                      '(KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'}
         self.answer = ''
+        self.result = ''
+        self.portal = name
 
     def save_pickle(self, data):
         with open('hh.pcl', 'wb') as f:
@@ -25,50 +31,68 @@ class GetHH():
 
     def get_data(self, url, header):
         self.answer = rs.get(url, headers=header)
-        self.save_pickle(self.answer.text)
+        # self.save_pickle(self.answer.text)
 
     def processing(self):
-        soup = bs(self.answer, "html.parser")
-        item_list = soup.find(attrs={"class": "vacancy-serp"})
-        all_class = []
-        for i in item_list.childGenerator():
-            if len(i['class']) > 1:
-                for j in i['class']:
-                    all_class.append(j)
-            else:
-                all_class.append(i['class'][0])
-        unique = []
-        for i in sorted(set(all_class)):
-            if 'vacancy' in i:
-                unique.append(i)
-        items = soup.findAll(True, {'class': unique})
-        return items
+        var_exit = 0
+        result_data = []
+        while 1 > 0:
+            soup = bs(self.answer.text, "html.parser")
+            item_list = soup.find(attrs={"class": "vacancy-serp"})
+            all_class = []
+            for i in item_list.childGenerator():
+                if len(i['class']) > 1:
+                    for j in i['class']:
+                        all_class.append(j)
+                else:
+                    all_class.append(i['class'][0])
+            unique = []
+            for i in sorted(set(all_class)):
+                if 'vacancy' in i:
+                    unique.append(i)
+            items = soup.findAll(True, {'class': unique})
+            for i in items:
+                cost = i.find(attrs={"class": "vacancy-serp-item__sidebar"}).text.replace('\u202f', '').split(' ')
+                if '–' in cost:
+                    min_cost = cost[0]
+                    max_cost = cost[2]
+                    unit = cost[3]
+                elif 'от' in cost:
+                    min_cost = cost[1]
+                    max_cost = 0
+                    unit = cost[2]
+                elif 'до' in cost:
+                    min_cost = 0
+                    max_cost = cost[1]
+                    unit = cost[2]
+                else:
+                    min_cost = 0
+                    max_cost = 0
+                    unit = 0
+                result_data.append(dict(zip(['Вакансия', 'Зарплата_нижний_порог', 'Зарплата_нижний_порог',
+                                             'Валюта', 'Ссылка', 'Сайт'],
+                                            [i.a.text, min_cost, max_cost, unit, i.a["href"], self.portal])))
+            try:
+                next_page = soup.find('a', {"data-qa": "pager-next"})['href']
+                var_exit += 1
+                txt = 'https://hh.ru' + next_page
+                self.get_data(str(txt), self.headers)
+                time.sleep(0.5)
+            except TypeError:
+                break
+        a = json.dumps(result_data, ensure_ascii=False, indent=2)
+        self.result = a
+        return a
 
     def save_data(self):
-        pass
+        with open('result.json', 'w', encoding='utf-8') as fa:
+            fa.write(self.result)
 
 
-my_hh = GetHH()
-my_hh.load_pickle()
-for i in my_hh.processing():
-    cost = i.find(attrs={"class": "vacancy-serp-item__sidebar"}).text.replace('\u202f', '').split(' ')
-    if '–' in cost:
-        min_cost = cost[0]
-        max_cost = cost[2]
-        unit = cost[3]
-    elif 'от' in cost:
-        min_cost = cost[1]
-        max_cost = 0
-        unit = cost[2]
-    elif 'до' in cost:
-        min_cost = 0
-        max_cost = cost[1]
-        unit = cost[2]
-    else:
-        min_cost = 0
-        max_cost = 0
-        unit = 0
+my_hh = GetHH('хехе.ру')
+my_hh.get_data(url_search, headers)
+my_hh.processing()
+my_hh.save_data()
 
-    print(f'Вакансия {i.a.text} , ссылка {i.a["href"]}, минимум {min_cost} максимум {max_cost} валюта {unit}')
-    cost = i.find(attrs={"class": "vacancy-serp-item__sidebar"}).text.replace('\u202f', '')
-    print(f'Вакансия {i.a.text} , ссылка {i.a["href"]},зп {cost}')
+
+#     print(f'Вакансия {i.a.text} , ссылка {i.a["href"]}, минимум {min_cost} максимум {max_cost} валюта {unit}')
