5510a2ea5ac1e24258e69a8c0c8da9b6300cedd1
Vladimir <79061188133@ya.ru>
2021-04-19 21:39:38 +0300

    функция выбора данных из коллекции

--- les03/mongo.py
+++ les03/mongo.py
@@ -10,7 +10,7 @@ def create_con():
     collection = db.hh
     return collection
 
-
+# вызов функции со второго урока для сбора данных и загрузки в монгу
 def load_to_db(collection):
     collection.insert_many(json.loads(v.start()))
 
@@ -18,34 +18,30 @@ def load_to_db(collection):
 def vac(min_cost=0, max_cost=0):
     # вывод значений без зп
     if min_cost == max_cost and min_cost == 0:
-        print(1)
         for i in create_con().find({'Зарплата_нижний_порог': {'$eq': min_cost},
                                     'Зарплата_верхний_порог': {'$eq': max_cost}}):
             print(i)
         return print('Вывод окончен')
     # если ввели не правильную пару
     elif min_cost > max_cost and max_cost != 0:
-        print(2)
         return print('Вы ввели не правильную пару значений')
     # вывод значение с минимальной зарплатой больше или равной минимальной ставки
     elif min_cost >= max_cost and max_cost == 0:
-        print(3)
         for i in create_con().find({'Зарплата_нижний_порог': {'$gte': min_cost}}):
             pprint(i)
         return print('Вывод окончен')
     # вывод всех значений
     elif min_cost == -1:
-        print(4)
         for i in create_con().find({}):
             print(i)
         return print('Вывод окончен')
     else:
-        print(5)
         # вывод по условию больше или равно нижнему порогу и меньше или равно верхнему
-        for i in create_con().find({'Зарплата_нижний_порог': {'$gte': min_cost}},
-                                   {'Зарплата_верхний_порог': {'$lte': max_cost}},
-                                   {'Зарплата_нижний_порог': {'$gt': 0}},
-                                   {'Зарплата_верхний_порог': {'$gt': 0}}):
+        for i in create_con().find({'Зарплата_нижний_порог': {'$gte': min_cost},
+                                    'Зарплата_верхний_порог': {'$lte': max_cost},
+                                    'Зарплата_нижний_порог': {'$gt': 0},
+                                    'Зарплата_верхний_порог': {'$gt': 0}
+                                    }):
             print(i)
     return print('Вывод окончен')
 
